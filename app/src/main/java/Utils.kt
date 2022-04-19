09d26e214c15600c46a7ecc7c9b2a0817fe02390
Slava Shumakov <shum.slav@yandex.ru>
2022-04-20 01:40:45 +0500

    schedule_day_card.xml

--- app/src/main/java/Utils.kt
+++ app/src/main/java/Utils.kt
@@ -1,5 +1,6 @@
 import android.content.Context
 import android.widget.Toast
+import java.util.*
 
 fun makeToast(context: Context, text:String){
     Toast.makeText(context,text,Toast.LENGTH_SHORT).show()
